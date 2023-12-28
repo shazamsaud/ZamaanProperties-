36ba7071a5b8c1a2b930665b00b0f9c4238857c1
shaz <shazamsaud1@gmail.com>
2023-12-28 15:34:41 +0530

    added signup ui

--- api/controller/auth-controller.js
+++ api/controller/auth-controller.js
@@ -4,7 +4,7 @@ import User from "../models/user-model.js";
 
 
 
-export const signup = async (req,res)=>{
+export const signup = async (req,res,next)=>{
 const { username, email, password } = req.body;
 const hashedpassword = bcryptjs.hashSync(password,10);
 const newUser = new User({username,email,password:hashedpassword})
@@ -13,7 +13,7 @@ try{
     await newUser.save();
     res.status(201).json("user created successfully")
 }catch(e){
-    res.status(500).json(e.message);
+    next(e);
 }
 }
 
