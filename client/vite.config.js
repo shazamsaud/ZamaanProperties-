19766b7ed78aca9df0ee8bce3996f5291f97a7c6
Shazamaan saud <115259555+shazamsaud@users.noreply.github.com>
2024-02-10 01:08:31 +0530

    Update vite.config.js https://zamaan-back.vercel.app/

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server :{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'https://zamaan-back.vercel.app/',
         secure:false,
       },
     },
